29764599e0e2f42255130be5baf073a88095fd1b
Dominic Tarr <dominic.tarr@gmail.com>
2014-09-19 00:49:08 +0300

    test with interleavings to find race conditions

--- test/errors.js
+++ test/errors.js
@@ -2,7 +2,6 @@
 //var tape = require('tape');
 var equal = require('deep-equal');
 var multicb = require('../')
-
 var t = require('assert')
 
 require('interleavings').test(function (isAsync) {
@@ -14,13 +13,10 @@ require('interleavings').test(function (isAsync) {
   var done = multicb()
   async(done(), 5, [null, 1])
   async(done(), 15, [null, 2])
-  async(done(), 10, [null, 3])
+  async(done(), 10, ['fail'])
   done(function(err, results) {
-    console.log('done')
-    t.equal(err, null)
-    t.equal(results[0][1], 1)
-    t.equal(results[1][1], 2)
-    t.equal(results[2][1], 3)
+    t.equal(err, 'fail')
+    t.equal(results, void 0)
     isAsync.done()
   })
 
